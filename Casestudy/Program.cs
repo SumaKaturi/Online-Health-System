e7d7d90433501ebdf76ed41905ab330f91725341
Sumanjali katuri <sumanjali.katuri@sonata-software.com>
2021-04-27 21:21:50 +0530

    Add project files.

---
+++ Casestudy/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Casestudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+}header a
+{
+    color:var(--black);
+
+}
+header a:hover
+{
+color: var(--blue);
+}
+header.cotainer.logo
+{
+    font - size:3rem;
+}
+header.cotainer.logo span
+{
+    color: var(--blue);
+}
+header.nav ul
+{
+    margin:0; padding:0;
+
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+    list-style:none;
+}
+header.nav ul li
+{
+    margin: 0 1rem;
+}
+header.nav ul li a
+{
+   font-size: 2rem;
+}
+
+
